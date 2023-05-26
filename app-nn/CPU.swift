feff653f4d3d6d378d66072e84bb2da4c634b501
buhe <bugu1986@126.com>
2023-05-26 09:52:46 +0800

    "Add CPU.swift file and PBXBuildFile reference for it in project.pbxproj."

---
+++ app-nn/CPU.swift
@@ -0,0 +1,15 @@
+//
+//  CPU.swift
+//  app-nn
+//
+//  Created by 顾艳华 on 2023/5/26.
+//
+
+import Foundation
+
+// n * m dot m * j = n * j
+func dot(a:[[Float]], b: [[Float]]) -> [[Float]] {
+    var result = [[Float]]()
+    
+    return result
+}
